e6d9b338f3881d4857a042e96ec334f464299b0f
dylan <meeusdylan@protonmail.com>
2019-05-20 00:04:35 +0200

    Add project euler example including range, filter, sum

--- examples/project_euler_1.go
+++ examples/project_euler_1.go
@@ -6,14 +6,27 @@ import (
 )
 
 func main() {
-	numbers := hasgo.Ints{}
-	var i int64
-	for i = 0; i < 1000; i++ {
-		numbers = append(numbers, i)
-	}
-	result := numbers.Filter(func(i int64) bool {
+	hasgoFlavour()
+	vanillaGo()
+}
+
+// The hasgo way of solving this problem
+func hasgoFlavour() {
+	result := hasgo.IntRange(0,1000).Filter(func(i int64) bool {
 		return i%3 == 0 || i%5 == 0
 	}).Sum()
 	fmt.Printf("%v\n", result)
-
 }
+
+// this is how you could solve this in normal Go code.
+func vanillaGo() {
+	var sum int
+	for i := 0; i < 1000; i++ {
+		if i % 3 == 0 || i % 5 == 0 {
+			sum += i
+		}
+	}
+	fmt.Printf("%v\n", sum)
+}
+
+
