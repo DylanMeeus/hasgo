27f920d9b30be1b6ff61c6ad7c97bfe864ddaee0
dylan <meeusdylan@protonmail.com>
2019-06-03 22:37:42 +0200

    Disable person example

--- examples/player_examples/main.go
+++ examples/player_examples/main.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"./player"
-	"fmt"
 )
 
 func main() {
-	p := player.Player{"Insanity", 1337}
-	p2 := player.Player{"Unknown", 2041}
-	ps := player.Players{p, p2}
-	fmt.Printf("%v\n", ps.Head())
 }
