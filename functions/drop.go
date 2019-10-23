ebc7df3ddaa90870db847876c34bb05030a15eb5
Chris Evans <chris@streamgo.co.uk>
2019-10-23 15:10:26 +0100

    Adds Drop godoc

--- functions/drop.go
+++ functions/drop.go
@@ -1,5 +1,7 @@
 package functions
 
+// Drop returns a new Slice with the elements after the provided key.
+// Can be generated for any type.
 func (s SliceType) Drop(i int) (out SliceType) {
 	for i < len(s) {
 		out = append(out, s[i])
