664798cc4a438b6b69e4cc82fe5a4f881920c46b
xcgpseud <chris.evans92@hotmail.com>
2019-10-28 13:13:44 +0000

    WIP

---
+++ functions/tails.go
@@ -0,0 +1,9 @@
+package functions
+
+// Tails returns all tails of a sequence, in order of large to small, as if it were called recursively.
+// Can be generated for any type.
+func (s SliceType) Tails() (out SliceSliceType) {
+	for i := len(s) - 1; i >= 0; i-- {
+
+	}
+}
