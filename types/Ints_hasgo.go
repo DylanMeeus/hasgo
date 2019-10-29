b32e51a9aa39480bc50b4f98184ddfae1a6658b2
xcgpseud <chris.evans92@hotmail.com>
2019-10-29 13:53:40 +0000

    Remove old comment as add README entry for SplitAt

--- types/Ints_hasgo.go
+++ types/Ints_hasgo.go
@@ -483,24 +483,6 @@ func (s Ints) SplitAt(i int) (before, after Ints) {
 	return
 }
 
-//package functions
-//
-//// SplitAt splits the slice at the given index, returning before and after as a tuple.
-//// Can be generated for any type.
-//func (s Ints) SplitAt(i int) (before, after Ints) {
-//	if i < 0 || i > len(s) {
-//		return
-//	}
-//	for k, v := range s {
-//		if k < i {
-//			before = append(before, v)
-//		} else {
-//			after = append(after, v)
-//		}
-//	}
-//	return
-//}
-
 // =============== sum.go =================
 
 // Sum returns the sum of all elements in the slice.
--- types/Strings_hasgo.go
+++ types/Strings_hasgo.go
@@ -411,24 +411,6 @@ func (s Strings) SplitAt(i int) (before, after Strings) {
 	return
 }
 
-//package functions
-//
-//// SplitAt splits the slice at the given index, returning before and after as a tuple.
-//// Can be generated for any type.
-//func (s Strings) SplitAt(i int) (before, after Strings) {
-//	if i < 0 || i > len(s) {
-//		return
-//	}
-//	for k, v := range s {
-//		if k < i {
-//			before = append(before, v)
-//		} else {
-//			after = append(after, v)
-//		}
-//	}
-//	return
-//}
-
 // =============== sum.go =================
 
 // Sum returns the sum of all elements in the slice.
